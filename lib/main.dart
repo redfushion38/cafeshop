34cca4b6130610f9a694cc56d56b5c1cb74e7717
Juan Gamboa <gamboapereira@hotmail.com>
2024-05-24 00:05:32 -0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:coffee_challenge/coffee_app.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+    ),
+  );
+  runApp(const CoffeeChallengeApp());
+}
+
